f5e334549af69f3f9b0375678339b7eb5752a16f
bhchtt <bohach.taras14@gmail.com>
2024-11-19 23:39:58 +0200

    Оновлення views, шаблонів та додавання нових медіа файлів

--- Portfolio/Portfolio/views.py
+++ Portfolio/Portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render  # Додайте цей рядок
+from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
